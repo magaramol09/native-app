cc59b9c3beaeca4d578acc37c79b9c812d2e7ed0
amol_magar <magaramol09@gmail.com>
2022-10-11 09:36:24 +0530

    initial development code

---
+++ src/shared/prisma/prisma.module.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+// src/prisma/prisma.module.ts
+import { Global, Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Global() // 👈 makes PrismaService globally available
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService], // 👈 export PrismaService for DI
+})
+export class PrismaModule {}
